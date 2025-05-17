39c7ff266ee96b1b841c694df79c8f551096160f
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-05-17 19:39:29 -0300

    Update findlib.sh

--- build/findlib.sh
+++ build/findlib.sh
@@ -1,1 +1,7 @@
+#! /bin/bash
 
+BIT=""
+
+if [ x"$2" == x"64" ]; then
+        BIT="64"
+        LIBDIRS="/lib64 /lib/x86_64-linux-gnu /lib /usr/lib64 /usr/lib/x86_64-linux-gnu /usr/lib /usr/local/lib /usr/lib/x86_64-linux-gnu/nss /usr/lib/nss"
